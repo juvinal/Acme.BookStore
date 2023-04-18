081cd3743fbd6687f1a5b27b6e3c8c0d92fd1b1b
Juvinal Alvarado <act.jalvarado@gmail.com>
2023-04-18 21:36:16 +0800

    added book to author relation

---
+++ src/Acme.BookStore.Blazor/Pages/Books.razor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Acme.BookStore.Authors;
+using Acme.BookStore.Permissions;
+using Blazorise;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Authorization;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Blazor.Pages;
+
+public partial class Books
+{
+	public Books() // Constructor
+	{
+		CreatePolicyName = BookStorePermissions.Books.Create;
+		UpdatePolicyName = BookStorePermissions.Books.Edit;
+		DeletePolicyName = BookStorePermissions.Books.Delete;
+	}
+	protected override async Task OnInitializedAsync()
+	{
+		await base.OnInitializedAsync();
+		authorList = (await AppService.GetAuthorLookupAsync()).Items;
+	}
+
+	protected override async Task OpenCreateModalAsync()
+	{
+		if (!authorList.Any())
+		{
+			throw new UserFriendlyException(message: L["AnAuthorIsRequiredForCreatingBook"]);
+		}
+
+		await base.OpenCreateModalAsync();
+		NewEntity.AuthorId = authorList.First().Id;
+	}
+}
