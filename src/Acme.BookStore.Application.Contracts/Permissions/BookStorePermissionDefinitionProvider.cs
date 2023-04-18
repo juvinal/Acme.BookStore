a01d92b6b888fee43f9d19c0ee86913ee69c64bc
Juvinal Alvarado <act.jalvarado@gmail.com>
2023-04-18 19:52:10 +0800

    added authorization

--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
@@ -6,15 +6,18 @@ namespace Acme.BookStore.Permissions;
 
 public class BookStorePermissionDefinitionProvider : PermissionDefinitionProvider
 {
-    public override void Define(IPermissionDefinitionContext context)
-    {
-        var myGroup = context.AddGroup(BookStorePermissions.GroupName);
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(BookStorePermissions.MyPermission1, L("Permission:MyPermission1"));
-    }
+	public override void Define(IPermissionDefinitionContext context)
+	{
+		var bookStoreGroup = context.AddGroup(BookStorePermissions.GroupName, L("Permission:BookStore"));
 
-    private static LocalizableString L(string name)
-    {
-        return LocalizableString.Create<BookStoreResource>(name);
-    }
+		var booksPermission = bookStoreGroup.AddPermission(BookStorePermissions.Books.Default, L("Permission:Books"));
+		booksPermission.AddChild(BookStorePermissions.Books.Create, L("Permission:Books.Create"));
+		booksPermission.AddChild(BookStorePermissions.Books.Edit, L("Permission:Books.Edit"));
+		booksPermission.AddChild(BookStorePermissions.Books.Delete, L("Permission:Books.Delete"));
+	}
+
+	private static LocalizableString L(string name)
+	{
+		return LocalizableString.Create<BookStoreResource>(name);
+	}
 }
